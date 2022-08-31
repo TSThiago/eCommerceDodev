ad20226c0cb53a63449874a32dff6513057db90e
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-30 22:38:09 -0300

    Criação da função que lista os produtos ordenados pelo id.

--- eCommerce.js
+++ eCommerce.js
@@ -2,42 +2,79 @@ idProdutoArray = []
 NomeProdutoArray = []
 PrecoProdutoArray = []
 AvaliacaoProdutoArray = []
-index = 0
 
-CadastrarProduto()
+idAux = 0
+nomeAux = 0
+precoAux = 0
+avaliacaoAux = 0
+
+index = 0
+continuar = 1
+
+while (continuar == 1) {
+    CadastrarProduto()
+    continuar = prompt("Aperte 2 para parar")
+}
 BuscarPeloId()
 BuscarPeloNome()
+ExibirPeloId()
 
-
-function CadastrarProduto(){
+function CadastrarProduto() {
     idProdutoArray[index] = prompt("Qual o id do produto?")
     NomeProdutoArray[index] = prompt("Qual o nome do produto?")
     PrecoProdutoArray[index] = prompt("Qual o preço do produto?")
     AvaliacaoProdutoArray[index] = prompt("Qual a avaliação do produto?")
-    console.log(idProdutoArray[index]+ " - " +NomeProdutoArray[index]+ " - " +PrecoProdutoArray[index]+ " - " +AvaliacaoProdutoArray[index])
+    console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
     index++
 }
 
-function BuscarPeloId(idParametro){
+function BuscarPeloId(idParametro) {
     idProduto = prompt("Qual o id do produto que você busca?")
-    for(index = 0; index < idProdutoArray.length; index++){
-        if(idProduto == idProdutoArray[index]){
-            console.log(idProdutoArray[index]+ " - " +NomeProdutoArray[index]+ " - " +PrecoProdutoArray[index]+ " - " +AvaliacaoProdutoArray[index])
+    for (index = 0; index < idProdutoArray.length; index++) {
+        if (idProduto == idProdutoArray[index]) {
+            console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
             return
-        }else{
+        } else {
             console.log("Produto não encontrado.")
         }
     }
 }
 
-function BuscarPeloNome(){
+function BuscarPeloNome() {
     nomeProduto = prompt("Qual o nome do produto que você busca?")
-    for(index = 0; index < NomeProdutoArray.length; index++){
-        if(nomeProduto == NomeProdutoArray[index]){
-            console.log("id: " +idProdutoArray[index])
+    for (index = 0; index < NomeProdutoArray.length; index++) {
+        if (nomeProduto == NomeProdutoArray[index]) {
+            console.log("id: " + idProdutoArray[index])
             return
-        }else{
+        } else {
             console.log("Produto não encontrado.")
         }
+    }
+}
+
+function ExibirPeloId() {
+    for (contador = 0; contador < idProdutoArray.length; contador++) {
+        for (index = 0; index < idProdutoArray.length - 1; index++) {
+            if (idProdutoArray[index] > idProdutoArray[index + 1]) {
+                idAux = idProdutoArray[index]
+                nomeAux = NomeProdutoArray[index]
+                precoAux = PrecoProdutoArray[index]
+                avaliacaoAux = AvaliacaoProdutoArray[index]
+
+                idProdutoArray[index] = idProdutoArray[index + 1]
+                NomeProdutoArray[index] = NomeProdutoArray[index + 1]
+                PrecoProdutoArray[index] = PrecoProdutoArray[index + 1]
+                AvaliacaoProdutoArray[index] = AvaliacaoProdutoArray[index + 1]
+
+                idProdutoArray[index + 1] = idAux
+                NomeProdutoArray[index + 1] = nomeAux
+                PrecoProdutoArray[index + 1] = precoAux
+                AvaliacaoProdutoArray[index + 1] = avaliacaoAux
+            }
+        }
+        index = 0
+    }
+    for (index = 0; index < idProdutoArray.length; index++) {
+        console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
     }
 }
