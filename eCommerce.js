764d3ade82237e8aee35475ed8c5a58cfaec118a
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-30 22:57:37 -0300

    Tentativa de criar a função 'ExibirPeloPreco'

--- eCommerce.js
+++ eCommerce.js
@@ -17,7 +17,7 @@ while (continuar == 1) {
 }
 BuscarPeloId()
 BuscarPeloNome()
-ExibirPeloId()
+ExibirPeloPreco()
 
 function CadastrarProduto() {
     idProdutoArray[index] = prompt("Qual o id do produto?")
@@ -75,6 +75,33 @@ function ExibirPeloId() {
         index = 0
     }
     for (index = 0; index < idProdutoArray.length; index++) {
+        console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
+    }
+}
+
+function ExibirPeloPreco(){
+    for (contador = 0; contador < PrecoProdutoArray.length; contador++) {
+        for (index = PrecoProdutoArray.length; index > 1; index--) {
+            if (PrecoProdutoArray[index] > PrecoProdutoArray[index - 1]) {
+                idAux = idProdutoArray[index]
+                nomeAux = NomeProdutoArray[index]
+                precoAux = PrecoProdutoArray[index]
+                avaliacaoAux = AvaliacaoProdutoArray[index]
+
+                idProdutoArray[index] = idProdutoArray[index - 1]
+                NomeProdutoArray[index] = NomeProdutoArray[index - 1]
+                PrecoProdutoArray[index] = PrecoProdutoArray[index - 1]
+                AvaliacaoProdutoArray[index] = AvaliacaoProdutoArray[index - 1]
+
+                idProdutoArray[index - 1] = idAux
+                NomeProdutoArray[index - 1] = nomeAux
+                PrecoProdutoArray[index - 1] = precoAux
+                AvaliacaoProdutoArray[index - 1] = avaliacaoAux
+            }
+        }
+        index = PrecoProdutoArray.length
+    }
+    for (index = 0; index < PrecoProdutoArray.length; index++) {
         console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
     }
 }
