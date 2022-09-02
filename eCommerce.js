c807fd507d512087c55147849fc367fef571c85a
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-01 21:38:02 -0300

    Criação da função que atualiza o preço de um produto escolhido pelo id, da função que ordena os produtos e da função que da o console.log dos dados do produto.

--- eCommerce.js
+++ eCommerce.js
@@ -15,14 +15,14 @@ while (continuar == 1) {
     CadastrarProduto()
     continuar = prompt("Aperte 1 para continuar \nAperte 2 para parar")
 }
-ExibirPelaAvaliacao()
+AtualizarPreco()
 
 function CadastrarProduto() {
     idProdutoArray[index] = parseInt(prompt("Qual o id do produto?"))
     NomeProdutoArray[index] = prompt("Qual o nome do produto?")
     PrecoProdutoArray[index] = parseInt(prompt("Qual o preço do produto?"))
     AvaliacaoProdutoArray[index] = parseInt(prompt("Qual a avaliação do produto?"))
-    console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
+    MostrarProdutos()
     index++
 }
 
@@ -30,7 +30,7 @@ function BuscarPeloId(idParametro) {
     idProduto = prompt("Qual o id do produto que você busca?")
     for (index = 0; index < idProdutoArray.length; index++) {
         if (idProduto == idProdutoArray[index]) {
-            console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
+            MostrarProdutos()
             return
         } else {
             console.log("Produto não encontrado.")
@@ -55,81 +55,74 @@ function ExibirPeloId() {
     for (contador = 0; contador < idProdutoArray.length; contador++) {
         for (index = 0; index < idProdutoArray.length - 1; index++) {
             if (idProdutoArray[index] > idProdutoArray[index + 1]) {
-                idAux = idProdutoArray[index]
-                nomeAux = NomeProdutoArray[index]
-                precoAux = PrecoProdutoArray[index]
-                avaliacaoAux = AvaliacaoProdutoArray[index]
-
-                idProdutoArray[index] = idProdutoArray[index + 1]
-                NomeProdutoArray[index] = NomeProdutoArray[index + 1]
-                PrecoProdutoArray[index] = PrecoProdutoArray[index + 1]
-                AvaliacaoProdutoArray[index] = AvaliacaoProdutoArray[index + 1]
-
-                idProdutoArray[index + 1] = idAux
-                NomeProdutoArray[index + 1] = nomeAux
-                PrecoProdutoArray[index + 1] = precoAux
-                AvaliacaoProdutoArray[index + 1] = avaliacaoAux
+                Ordenar()
             }
         }
         index = 0
     }
     for (index = 0; index < idProdutoArray.length; index++) {
-        console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
+        MostrarProdutos()
     }
 }
 
 function ExibirPeloPreco() {
     console.log("Ordenando pelo preço:")
-    for(contador = 0; contador < PrecoProdutoArray.length; contador++){
-        for(index = 0; index < PrecoProdutoArray.length; index++){
-            if(PrecoProdutoArray[index] < PrecoProdutoArray[index + 1]){
-                idAux = idProdutoArray[index]
-                nomeAux = NomeProdutoArray[index]
-                precoAux = PrecoProdutoArray[index]
-                avaliacaoAux = AvaliacaoProdutoArray[index]
-
-                idProdutoArray[index] = idProdutoArray[index + 1]
-                NomeProdutoArray[index] = NomeProdutoArray[index + 1]
-                PrecoProdutoArray[index] = PrecoProdutoArray[index + 1]
-                AvaliacaoProdutoArray[index] = AvaliacaoProdutoArray[index + 1]
-
-                idProdutoArray[index + 1] = idAux
-                NomeProdutoArray[index + 1] = nomeAux
-                PrecoProdutoArray[index + 1] = precoAux
-                AvaliacaoProdutoArray[index + 1] = avaliacaoAux
+    for (contador = 0; contador < PrecoProdutoArray.length; contador++) {
+        for (index = 0; index < PrecoProdutoArray.length - 1; index++) {
+            if (PrecoProdutoArray[index] < PrecoProdutoArray[index + 1]) {
+                Ordenar()
             }
         }
         index = 0
     }
     for (index = 0; index < PrecoProdutoArray.length; index++) {
-        console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
+        MostrarProdutos()
     }
 }
 
-function ExibirPelaAvaliacao(){
+function ExibirPelaAvaliacao() {
     console.log("Ordenando pela avaliação:")
-    for(contador = 0; contador < AvaliacaoProdutoArray.length; contador++){
-        for(index = 0; index < AvaliacaoProdutoArray.length - 1; index++){
-            if(AvaliacaoProdutoArray[index] < AvaliacaoProdutoArray[index + 1]){
-                idAux = idProdutoArray[index]
-                nomeAux = NomeProdutoArray[index]
-                precoAux = PrecoProdutoArray[index]
-                avaliacaoAux = AvaliacaoProdutoArray[index]
-            
-                idProdutoArray[index] = idProdutoArray[index + 1]
-                NomeProdutoArray[index] = NomeProdutoArray[index + 1]
-                PrecoProdutoArray[index] = PrecoProdutoArray[index + 1]
-                AvaliacaoProdutoArray[index] = AvaliacaoProdutoArray[index + 1]
-            
-                idProdutoArray[index + 1] = idAux
-                NomeProdutoArray[index + 1] = nomeAux
-                PrecoProdutoArray[index + 1] = precoAux
-                AvaliacaoProdutoArray[index + 1] = avaliacaoAux
+    for (contador = 0; contador < AvaliacaoProdutoArray.length; contador++) {
+        for (index = 0; index < AvaliacaoProdutoArray.length - 1; index++) {
+            if (AvaliacaoProdutoArray[index] < AvaliacaoProdutoArray[index + 1]) {
+                Ordenar()
             }
         }
         index = 0
     }
     for (index = 0; index < AvaliacaoProdutoArray.length; index++) {
-        console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - " + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
+        MostrarProdutos()
     }
+}
+
+function AtualizarPreco(){
+    idProduto = prompt("Qual o id do produto?")
+    for(index = 0; index < idProdutoArray.length; index++){
+        if(idProduto == idProdutoArray[index]){
+            console.log("Produto " +NomeProdutoArray[index]+ ":")
+            PrecoProdutoArray[index] = parseFloat(prompt("Insira o novo preço do produto"))
+            MostrarProdutos()
+        }
+    }
+}
+
+function Ordenar() {
+    idAux = idProdutoArray[index]
+    nomeAux = NomeProdutoArray[index]
+    precoAux = PrecoProdutoArray[index]
+    avaliacaoAux = AvaliacaoProdutoArray[index]
+
+    idProdutoArray[index] = idProdutoArray[index + 1]
+    NomeProdutoArray[index] = NomeProdutoArray[index + 1]
+    PrecoProdutoArray[index] = PrecoProdutoArray[index + 1]
+    AvaliacaoProdutoArray[index] = AvaliacaoProdutoArray[index + 1]
+
+    idProdutoArray[index + 1] = idAux
+    NomeProdutoArray[index + 1] = nomeAux
+    PrecoProdutoArray[index + 1] = precoAux
+    AvaliacaoProdutoArray[index + 1] = avaliacaoAux
+}
+
+function MostrarProdutos(){
+    console.log(idProdutoArray[index] + " - " + NomeProdutoArray[index] + " - R$" + PrecoProdutoArray[index] + " - " + AvaliacaoProdutoArray[index])
 }
